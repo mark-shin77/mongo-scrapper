e565576e343d0dbd6bb6b3c5b1e90f6a508423f9
Mark <contact.mshin@gmail.com>
2019-01-07 18:16:45 -0700

    building server

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 // Dependencies
-var express = require("express");
-var exphbs = require("express-handlebars");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
+    // Server Setup
+    var express = require("express");
+    var exphbs = require("express-handlebars");
+    // Status Logger (Middleware)
+    var logger = require("morgan");
+    // Database
+    var mongoose = require("mongoose");
+    // Scraper
+    var axios = require("axios");
+    var cheerio = require("cheerio");
+
+// Require Models needed for app
+    var db = require("./models");
+// Require Routes 
+    require("./routes/routes");
+
+// Local host Port
+    var PORT = process.env.PORT || 4000;
+
+// Initialize Express
+    var app = express();
+
+// Setting morgan logger for logging request
+    app.use(logger('dev'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+// Making public default static location
+    app.use(express.static("public"));
+
+// Handlebars
+    app.engine("handlebars", exphbs ({ defaultLayout: "main"}));
+    app.set("view engine", "handlebars");
+
+// Connect to Mongo DB
+    mongoose.connect('mongodb://localhost/mongo-scrapper', { useNewUrlParser: true });
+
+// Starting Server
+    app.listen(PORT, () => console.log("App running on port " + PORT + "!"));
