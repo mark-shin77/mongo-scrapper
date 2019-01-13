78083e0b962f418e5df8948f77ccd9f1679a116b
Mark <contact.mshin@gmail.com>
2019-01-13 14:11:40 -0700

    file structure

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -6,6 +6,7 @@ $(document).on("click", ".btn.save", function(){
     url: "/jobs/save/" + thisId
   }).then(function(){
     alert("Job has been saved!")
+    location.reload();
   })
 })
 
@@ -22,7 +23,32 @@ $(document).on("click", ".btn.delete", function(){
 })
 
 // Add note button
-$(document).on("click", "", function(){})
+$(document).on("click", ".btn.notes", function(){
+
+})
 
 
 $(document).on("click", "", function(){})
+
+// Alert showing user there are no saved jobs
+function noSavedJobs (){
+  var notification = $(
+    "<div class='alert alert-warning text-center'> <h4>Uh Oh! Looks like we don't have any saved articles.</h4>" +
+    "<h5><a href='/scrape' class='no-jobs-notification'>Click here to look through available jobs!</a></h5></div>"
+  )
+  $(".saved-jobs-container").append(notification);
+}
+var jobs = $(".jobs")
+if (jobs.length == 0) {
+  noSavedJobs();
+  needToScrape();
+}
+
+// Prompt telling user to scrape jobs
+function needToScrape () {
+  var notification = $(
+    "<div class='alert alert-warning text-center'> <h4>Uh Oh! Scrape to find available Jobs!</h4>" +
+    "<h5><a href='/scrape' class='no-jobs-notification'>Click here to find available jobs!</a></h5></div>"
+  )
+  $(".job-wrapper").append(notification);
+}
