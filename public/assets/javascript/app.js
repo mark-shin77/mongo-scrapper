edc569966bd6d2d5e01689d33d38dddbeb863f1c
Mark <contact.mshin@gmail.com>
2019-01-13 17:08:48 -0700

    notes posting

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,14 +1,14 @@
 // Job Save Button
 $(document).on("click", ".btn.save", function(){
-  var thisId = $(this).attr("data-id")
+  var thisId = $(this).attr("data-id");
   $.ajax ({
     method: "POST",
     url: "/jobs/save/" + thisId
   }).then(function(){
     alert("Job has been saved!")
     location.reload();
-  })
-})
+  });
+});
 
 // Job Delete Button
 $(document).on("click", ".btn.delete", function(){
@@ -20,15 +20,26 @@ $(document).on("click", ".btn.delete", function(){
     console.log("Job Deleted")
     location.reload();
   });
-})
+});
 
 // Add note button
-$(document).on("click", ".btn.notes", function(){
-
-})
-
-
-$(document).on("click", "", function(){})
+$(document).on("click", ".btn.save-note", function(){
+  var newNote = $(".user-note").val();
+  var newNoteContainer = $(
+    "<div class='row'>" +
+      "<div class='new-notes col-10'><p>"+ newNote +"</p></div>" +
+      "<button type='button' class='btn btn-danger delete-note'> X </button>" +
+    "</div>"
+  )
+  var thisId = $(this).attr("data-id");
+  $.ajax ({
+    method: "POST",
+    url: "/jobs/" + thisId
+  }).then(function(){
+    $(".notes-container").append(newNoteContainer)
+    $("#user-text-input").val("");
+  })
+});
 
 // Alert showing user there are no saved jobs
 function noSavedJobs (){
@@ -37,12 +48,7 @@ function noSavedJobs (){
     "<h5><a href='/scrape' class='no-jobs-notification'>Click here to look through available jobs!</a></h5></div>"
   )
   $(".saved-jobs-container").append(notification);
-}
-var jobs = $(".jobs")
-if (jobs.length == 0) {
-  noSavedJobs();
-  needToScrape();
-}
+};
 
 // Prompt telling user to scrape jobs
 function needToScrape () {
@@ -51,4 +57,10 @@ function needToScrape () {
     "<h5><a href='/scrape' class='no-jobs-notification'>Click here to find available jobs!</a></h5></div>"
   )
   $(".job-wrapper").append(notification);
-}
+};
+
+var jobs = $(".jobs")
+if (jobs.length == 0) {
+  noSavedJobs();
+  needToScrape();
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-    // Our scraping tools
+// Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
 var axios = require("axios");
